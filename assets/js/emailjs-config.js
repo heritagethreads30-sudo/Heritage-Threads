c18161154aeb936421629964ddae6d50e13dd57e
heritagethreads30-sudo <heritagethreads30@gmail.com>
2026-03-26 14:03:50 -0700

    Delete assets directory

--- assets/js/emailjs-config.js
+++
@@ -1,6 +0,0 @@
-window.EMAILJS_CONFIG = {
-  publicKey: "58P-Ol9hKHrZENqP5",
-  serviceId: "service_njjway9",
-  verificationTemplateId: "template_1xv6o9x", 
-  contactTemplateId: "template_uc76is1"       
-};
--- assets/js/sheets-config.js
+++
@@ -1,4 +0,0 @@
-window.SHEETS_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwnFnWkN5CaSTxGrWGWnjC1bZ1QfZJ-QoieHDrHdRnlpsto6J7u-kVgkWrgR00NcisbJQ/exec",
-  adminEmail: "heritagethreads30@gmail.com"
-};
